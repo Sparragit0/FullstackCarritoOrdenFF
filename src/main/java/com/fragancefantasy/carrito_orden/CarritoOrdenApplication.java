3a315d7eda571b10f2f0dbcb15fc3d0f3e2d3dc5
Franco Tamagnone <fr.tamagnone@duocuc.cl>
2025-11-19 02:23:55 -0300

    modelos

---
+++ src/main/java/com/fragancefantasy/carrito_orden/CarritoOrdenApplication.java
@@ -0,0 +1,13 @@
+package com.fragancefantasy.carrito_orden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarritoOrdenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarritoOrdenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fragancefantasy/carrito_orden/CarritoOrdenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fragancefantasy.carrito_orden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarritoOrdenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
