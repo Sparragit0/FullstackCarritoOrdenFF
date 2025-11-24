1ed35bb8039c4e8927b4dab7b22a8f7b59966aba
Franco Tamagnone <fr.tamagnone@duocuc.cl>
2025-11-23 22:22:54 -0300

    v1

---
+++ src/main/java/com/fragancefantasy/carrito_orden/repository/OrdenRepository.java
@@ -0,0 +1,16 @@
+package com.fragancefantasy.carrito_orden.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fragancefantasy.carrito_orden.model.EstadoOrden;
+import com.fragancefantasy.carrito_orden.model.Orden;
+
+public interface OrdenRepository extends JpaRepository<Orden,Long>{
+    // Obtener historial de un usuario
+    List<Orden> findByUsuarioIdOrderByFechaCreacion(String usuarioId);
+
+    // Filtrar x estado
+    List<Orden> findByEstado(EstadoOrden estado);
+}
