5fa4a5bcbadb8e8773719b36755a51a27b965d35
Sparragit0 <fr.tamagnone@duocuc.cl>
2025-12-05 11:27:42 -0300

    Update OrdenController.java

--- src/main/java/com/fragancefantasy/carrito_orden/controller/OrdenController.java
+++ src/main/java/com/fragancefantasy/carrito_orden/controller/OrdenController.java
@@ -27,7 +27,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/ordenes")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:5173",
+    "http://3.216.102.70")
 public class OrdenController {
     private final OrdenService ordenService;
     private final JwtService jwtService;
